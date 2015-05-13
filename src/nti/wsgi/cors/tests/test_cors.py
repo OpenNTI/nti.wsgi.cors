68f7980a6456efb874d7c4412d5a7d6e2c67c055
Jason Madden <jason.madden@nextthought.com>
2015-05-13 14:31:18 +0000

    Compat with zope-testrunner.
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.wsgi.cors/trunk@65167 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/wsgi/cors/tests/test_cors.py
+++ src/nti/wsgi/cors/tests/test_cors.py
@@ -18,9 +18,7 @@ logger = __import__('logging').getLogger(__name__)
 from hamcrest import assert_that
 from hamcrest import is_
 from hamcrest import has_key
-from hamcrest import has_entry
-
-import nti.testing.base
+import unittest
 
 from webtest import TestApp
 
@@ -29,43 +27,45 @@ from ..cors import cors_option_filter_factory
 
 from paste.exceptions.errormiddleware import ErrorMiddleware
 
-def test_expected_exceptions_still_have_cors():
+class TestCors(unittest.TestCase):
 
-	def raises_app( environ, start_response ):
-		raise IOError('bad')
+	def test_expected_exceptions_still_have_cors(self):
 
-	catching_app = ErrorMiddleware( raises_app )
-	app = cors_filter_factory( catching_app )
+		def raises_app( environ, start_response ):
+			raise IOError('bad')
 
-	testapp = TestApp( app )
-	res = testapp.get( b'/the_path_doesnt_matter', status=500 )
+		catching_app = ErrorMiddleware( raises_app )
+		app = cors_filter_factory( catching_app )
 
-	assert_that( res.normal_body, is_( 'Failed to handle request bad') )
+		testapp = TestApp( app )
+		res = testapp.get( b'/the_path_doesnt_matter', status=500 )
 
-	# Errors set the right response headers
-	res = testapp.get( b'/',
-					   extra_environ={b'HTTP_ORIGIN': b'http://example.org'},
-					   status=500 )
-	assert_that( res.headers, has_key( 'Access-Control-Allow-Origin' ) )
+		assert_that( res.normal_body, is_( 'Failed to handle request bad') )
 
-def test_option_handler():
-
-	def raises_app( environ, start_response ):
-		raise IOError('bad')
-
-	option_app = cors_option_filter_factory( raises_app )
-	app = cors_filter_factory( option_app )
-
-
-	testapp = TestApp( app )
-	res = testapp.options( b'/the_path_doesnt_matter',
+		# Errors set the right response headers
+		res = testapp.get( b'/',
 						   extra_environ={b'HTTP_ORIGIN': b'http://example.org'},
-						   status=200 )
+						   status=500 )
+		assert_that( res.headers, has_key( 'Access-Control-Allow-Origin' ) )
 
-	assert_that( res.headers, has_key( 'Access-Control-Allow-Methods' ) )
+	def test_option_handler(self):
 
-	# Non-options pass through
-	res = testapp.get( b'/',
-					   extra_environ={b'HTTP_ORIGIN': b'http://example.org'},
-					   status=500 )
-	assert_that( res.headers, has_key( 'Access-Control-Allow-Origin' ) )
+		def raises_app( environ, start_response ):
+			raise IOError('bad')
+
+		option_app = cors_option_filter_factory( raises_app )
+		app = cors_filter_factory( option_app )
+
+
+		testapp = TestApp( app )
+		res = testapp.options( b'/the_path_doesnt_matter',
+							   extra_environ={b'HTTP_ORIGIN': b'http://example.org'},
+							   status=200 )
+
+		assert_that( res.headers, has_key( 'Access-Control-Allow-Methods' ) )
+
+		# Non-options pass through
+		res = testapp.get( b'/',
+						   extra_environ={b'HTTP_ORIGIN': b'http://example.org'},
+						   status=500 )
+		assert_that( res.headers, has_key( 'Access-Control-Allow-Origin' ) )
